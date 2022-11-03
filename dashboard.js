19dce30b52caf6665ff7c327d3bd78a0a3d43740
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-11-03 20:57:49 -0300

    Commit - Lista Contatos
    Lista adiciona o VIP mas não exibe

--- dashboard.js
+++ dashboard.js
@@ -17,53 +17,61 @@ $(document).ready(() => {
 });
 
 const regEventClickTr = () => {
-    $("tr").click((e) => {
-        let tr = $(e.target.parentElement);
-        let regLinha = tr.find("td");
-        let tdId = regLinha[0];
-        $("#id").val($(tdId).html());
-        $("#nome").val($(regLinha[1]).html());
-        $("#email").val($(regLinha[2]).html());
-      });
+  $("tr").click((e) => {
+    let tr = $(e.target.parentElement);
+    let regLinha = tr.find("td");
+    let tdId = regLinha[0];
+    $("#id").val($(tdId).html());
+    $("#nome").val($(regLinha[1]).html());
+    $("#email").val($(regLinha[2]).html());
+    $("#idade").val($(regLinha[3]).html());
+    $("#status").val($(reglinha[4]).html());
+  });
 }
 
 const salvarContato = () => {
+  let id = $("#id");
   let nome = $("#nome");
   let email = $("#email");
-  let id = $("#id");
+  let idade = $("#idade");
+  let status = $("#status");
   let tamLista = listaContatos == null ? 0 : listaContatos.length;
-  let contato
+  let contato;
   let msg = "Por favor, preencher todos os campos...";
-  
+
   if (listaContatos == null) {
-      listaContatos = new Array();
-    }
-    
-    if (nome.val() === "" || email.val() === "") {
-        $("#msg .alert").html(`<h3>${msg}</h3>`);
-        $(".toast-body").html(msg);
-        $("#msg .alert").fadeIn("slow", async () => {
-            await delay(3000);
-            $("#msg .alert").fadeOut("slow");
-        });
-        const toastLive = document.getElementById("liveToast");
-        const toast = new bootstrap.Toast(toastLive);
-        toast.show();
-        return;
-    }
+    listaContatos = new Array();
+  }
 
-    contato = { id: 0, 
-        nome: nome.val(), 
-        email: email.val() };
+  if (nome.val() === "" || email.val() === "") {
+    $("#msg .alert").html(`<h3>${msg}</h3>`);
+    $(".toast-body").html(msg);
+    $("#msg .alert").fadeIn("slow", async () => {
+      await delay(3000);
+      $("#msg .alert").fadeOut("slow");
+    });
+    const toastLive = document.getElementById("liveToast");
+    const toast = new bootstrap.Toast(toastLive);
+    toast.show();
+    return;
+  }
 
-    if (id.val() !== "") {
-        var i = listaContatos.findIndex((contato) => contato.id === id.val());
-        contato.id = id.val();
-        listaContatos.splice(i,1,contato);
-    } else {
-        let idInsert = tamLista + 1;
-        contato.id = idInsert;
-        listaContatos.push(contato);
+  contato = {
+    id: 0,
+    nome: nome.val(),
+    email: email.val(),
+    idade: idade.val(),
+    status: status.val()
+  };
+
+  if (id.val() !== "") {
+    var i = listaContatos.findIndex((contato) => contato.id === id.val());
+    contato.id = id.val();
+    listaContatos.splice(i, 1, contato);
+  } else {
+    let idInsert = tamLista + 1;
+    contato.id = idInsert;
+    listaContatos.push(contato);
   }
 
   setJsonItem(LISTA_CONTATOS, listaContatos);
@@ -84,6 +92,8 @@ const listarContatos = () => {
     var colId = document.createElement("td");
     var colNome = document.createElement("td");
     var colEmail = document.createElement("td");
+    var colIdade = document.createElement("td");
+    var colStatus = document.createElement("td");
     var colActions = document.createElement("td");
     $(colActions).html(`<div class="d-flex">
         <button onclick="removeItemList(${c.id});" 
@@ -93,7 +103,9 @@ const listarContatos = () => {
     $(colId).html(c.id);
     $(colNome).html(c.nome);
     $(colEmail).html(c.email);
-    $(linha).append(colId).append(colNome).append(colEmail).append(colActions);
+    $(colIdade).html(c.email);
+    $(colStatus).html(c.status);
+    $(linha).append(colId).append(colNome).append(colEmail).append(colIdade).append(colStatus).append(colActions);
     listaHTML.append(linha);
   });
 
@@ -108,13 +120,14 @@ const removeItemList = (id) => {
   listarContatos();
 };
 
-const limpaCampos = () =>{
-    $("#id").val("");
-    $("#nome").val("");
-    $("#email").val("");
+const limpaCampos = () => {
+  $("#id").val("");
+  $("#nome").val("");
+  $("#email").val("");
+  $("#idade").val("");
 }
 
-function verificaLogin () {
+function verificaLogin() {
   if (getItem('userLogado')) {
     return true
   } else {
